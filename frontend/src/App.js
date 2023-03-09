aafd0ef801ecaeab34d903c11bd73307d7ea9000
sanglim <sanglim@kookmin.ac.kr>
2023-03-09 17:37:54 +0900

    style. 웹 max 값 설정

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,9 +4,21 @@ import Login from './pages/Login/Login';
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Login />} />
-    </Routes>
+    <div
+      className="App"
+      style={{
+        maxWidth: '480px',
+        margin: '0 auto',
+        minHeight: '100%',
+        width: 'auto',
+        position: 'relative',
+        wordBreak: 'keep-all',
+      }}
+    >
+      <Routes>
+        <Route path="/" element={<Login />} />
+      </Routes>
+    </div>
   );
 }
 
