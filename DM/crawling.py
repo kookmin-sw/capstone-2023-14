55425c221e7c96a27d89b96a2f97ceab2c0dfb36
young43 <seo5220@kookmin.ac.kr>
2023-03-20 19:19:43 +0900

    주석 및 로그출력

--- DM/crawling.py
+++ DM/crawling.py
@@ -36,73 +36,78 @@ def text_scraping(url):
 
 
 
-# load .env
-load_dotenv()
-client_id = os.environ.get('CLIENT_ID')
-client_secret = os.environ.get('CLIENT_SECRET')
 
 
-headers = {
-    'X-Naver-Client-Id': client_id,
-    'X-Naver-Client-Secret': client_secret,
-}
-
-# 도시 파일관련 json 가져오기
-file_path = './country.json'
-with open(file_path, 'r', encoding='UTF-8') as file:
-    data = json.load(file)['item']
-
-country_list = []
-for item in data:
-    country_list.extend(item['country'])
-
-# 진행된 정보 가져오기 (파일명 제일 앞에있는 번호를 기준)
-crawl_dir = './blog_data'
-file_list = os.listdir('./blog_data')
-last_index = 0
-if len(file_list) > 0:
-    last_index = sorted(file_list, key=lambda x: int(x.split('_')[0]), reverse=True)[0].split('_')[0]
-    last_index = int(last_index)
-    print(f"{last_index}_{country_list[last_index]} 까지 진행했음")
-    print('='*50)
+if __name__ == "__main__":
+    # load .env
+    load_dotenv()
+    client_id = os.environ.get('CLIENT_ID')
+    client_secret = os.environ.get('CLIENT_SECRET')
 
 
+    headers = {
+        'X-Naver-Client-Id': client_id,
+        'X-Naver-Client-Secret': client_secret,
+    }
 
-for idx, country in enumerate(country_list):
-    if idx <= last_index: continue
+    # 도시 파일관련 json 가져오기
+    file_path = './country.json'
+    with open(file_path, 'r', encoding='UTF-8') as file:
+        data = json.load(file)['item']
 
-    review_list = []
-    # 네이버 검색 API는 최대 100개까지 밖에 볼 수 없기때문에 for문을 돌려서 총 1000개의 블로그를 조회
-    for i in range(1, 11):
-        params = {
-            'start': i,
-            'display': 100,
-            'sort': 'sim',
-            'query': f'{country} 여행'
-        }
+    country_list = []
+    for item in data:
+        country_list.extend(item['country'])
+
+    # 진행된 정보 가져오기 (파일명 제일 앞에있는 번호를 기준)
+    crawl_dir = './blog_data'
+    file_list = os.listdir('./blog_data')
+    last_index = 0
+
+    if len(file_list) > 0:
+        last_index = sorted(file_list, key=lambda x: int(x.split('_')[0]), reverse=True)[0].split('_')[0]
+        last_index = int(last_index)
+        print(f"{last_index}_{country_list[last_index]} 까지 진행했음")
+        print('='*50)
+    # print(f"Last Index : {last_index}")
 
 
-        url = "https://openapi.naver.com/v1/search/blog"
-        res = requests.get(url, headers=headers, params=params)
-        res.encoding = 'utf-8'
-        resJson = res.json()
+    for idx, country in enumerate(country_list):
+        if last_index > 0 and idx <= last_index: continue
+        print(f"=============== {idx}_{country} 진행중 ===============")
 
-        for item in resJson['items']:
-            title = item['title'].replace("<b>", "").replace("</b>", "")
-            post_link = item['link']
+        review_list = []
+        # 네이버 검색 API는 최대 100개까지 밖에 볼 수 없기때문에 for문을 돌려서 총 1000개의 블로그를 조회
+        for i in range(1, 11):
+            params = {
+                'start': i,
+                'display': 100,
+                'sort': 'sim',
+                'query': f'{country} 여행'
+            }
 
-            blog_p = re.compile("blog.naver.com")
-            blog_m = blog_p.search(post_link)
 
-            if blog_m:
-                # iframe 제거 후 블로그 내용 가져옴
-                blog_text = text_scraping(delete_iframe(post_link))
-                review_list.append(blog_text)
+            url = "https://openapi.naver.com/v1/search/blog"
+            res = requests.get(url, headers=headers, params=params)
+            res.encoding = 'utf-8'
+            resJson = res.json()
 
-    # Pandas dataframe으로 변환 -> CSV 파일 저장
-    df = pd.DataFrame({'country': [country for i in review_list], 'contents' : review_list})
-    save_file_path = f'./blog_data/{idx}_{country}_crawling.csv'
-    df.to_csv(save_file_path, encoding='UTF-8')
+            for item in resJson['items']:
+                title = item['title'].replace("<b>", "").replace("</b>", "")
+                post_link = item['link']
 
-    print(f'Saved {save_file_path}')
-    print('='*50)
+                blog_p = re.compile("blog.naver.com")
+                blog_m = blog_p.search(post_link)
+
+                if blog_m:
+                    # iframe 제거 후 블로그 내용 가져옴
+                    blog_text = text_scraping(delete_iframe(post_link))
+                    review_list.append(blog_text)
+
+        # Pandas dataframe으로 변환 -> CSV 파일 저장
+        df = pd.DataFrame({'country': [country for i in review_list], 'contents' : review_list})
+        save_file_path = f'./blog_data/{idx}_{country}_crawling.csv'
+        df.to_csv(save_file_path, encoding='UTF-8')
+
+        print(f'Saved {save_file_path}')
+        print('='*50)
