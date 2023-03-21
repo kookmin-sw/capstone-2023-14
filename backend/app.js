5f495ee9bd3abb8fa3c24e4c4bf97bb0b8b058cc
kimjihong9 <wlghddl9@gmail.com>
2023-03-22 04:47:36 +0900

    backend setup, gitignore 생성

---
+++ backend/app.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const app = express();
+
+// Middleware
+app.use(express.json()); // JSON 형식의 요청 처리
+app.use(express.urlencoded({ extended: true })); // URL 인코딩된 요청 처리
+
+// Routes
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+// Server
+const PORT = process.env.PORT || 5001;
+app.listen(PORT, () => {
+  console.log(`Server started on port ${PORT}`);
+});
