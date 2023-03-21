6218fcc546e40011bbc0645c12f279a286ec54ea
kimjihong9 <wlghddl9@gmail.com>
2023-03-22 03:22:17 +0900

    feat. slider 컴포넌트 생성

--- frontend/src/pages/Detail/index.js
+++ frontend/src/pages/Detail/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Header from '../../components/Header/header';
 import Footer from '../../components/Footer/footer';
 import { useParams } from 'react-router-dom';
+import ImgSlider from '../../components/Slider';
 
 function Detail() {
   const params = useParams();
@@ -9,8 +10,10 @@ function Detail() {
 
   return (
     <div>
+      {' '}
       <Header />
-      {destination}
+      <h1>{destination}</h1>
+      <ImgSlider />
       <Footer />
     </div>
   );
