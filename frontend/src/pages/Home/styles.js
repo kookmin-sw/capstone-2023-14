4efc9aa6df9a55260c6702849edb03b0f851f29f
kimjihong9 <wlghddl9@gmail.com>
2023-03-21 16:10:55 +0900

    fix. home page responsive-style 적용

--- frontend/src/pages/Home/styles.js
+++ frontend/src/pages/Home/styles.js
@@ -1,6 +1,11 @@
 import styled from 'styled-components';
 
 export const Wrap = styled.div`
-  width: 432px;
   margin: 0 auto;
+  position: relative;
+  padding: 0 24px;
+
+  @media screen and (max-width: 400px) {
+    padding: 0 20px;
+  }
 `;
