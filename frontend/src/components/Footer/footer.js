7354d65921b8c19d4fd6bfdb9dda988581453ef2
sanglim <sanglim@kookmin.ac.kr>
2023-03-23 16:41:24 +0900

    feat. footer 아이콘 이미지 적용

--- frontend/src/components/Footer/footer.js
+++ frontend/src/components/Footer/footer.js
@@ -1,7 +1,32 @@
 import React from 'react';
 import { Wrap } from './styles';
+import { useNavigate } from 'react-router-dom';
 
 function Footer() {
-  return <Wrap></Wrap>;
+  const navigator = useNavigate();
+  return (
+    <Wrap>
+      <img
+        src={process.env.PUBLIC_URL + '/images/Footer/homeIcon.svg'}
+        onClick={() => navigator('/home')}
+      />
+      <img
+        src={process.env.PUBLIC_URL + '/images/Footer/recordIcon.svg'}
+        onClick={() => navigator('/')}
+      />
+      <img
+        src={process.env.PUBLIC_URL + '/images/Footer/boardIcon.svg'}
+        onClick={() => navigator('/')}
+      />
+      <img
+        src={process.env.PUBLIC_URL + '/images/Footer/notiIcon.svg'}
+        onClick={() => navigator('/')}
+      />
+      <img
+        src={process.env.PUBLIC_URL + '/images/Footer/mypageIcon.svg'}
+        onClick={() => navigator('/')}
+      />
+    </Wrap>
+  );
 }
 export default Footer;
