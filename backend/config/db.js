a4dd783aff33bcf0300e79afdb38bc8b27e3a0c3
kimjihong9 <wlghddl9@gmail.com>
2023-03-26 04:58:26 +0900

    feat. db 연결

---
+++ backend/config/db.js
@@ -0,0 +1,25 @@
+const mysql = require('mysql');
+
+const dbHost = process.env.DB_IP;
+const dbName = process.env.DB_NAME;
+const dbPort = process.env.DB_PORT;
+const dbUser = process.env.DB_USER;
+const dbPassword = process.env.DB_PASSWD;
+
+const db = mysql.createConnection({
+  host: dbHost,
+  user: dbUser,
+  port: dbPort,
+  password: dbPassword,
+  database: dbName,
+});
+
+db.connect((err) => {
+  if (err) {
+    console.log('db 연결 오류', err);
+    return;
+  }
+  console.log('db 연결 성공');
+});
+
+module.exports = db;
