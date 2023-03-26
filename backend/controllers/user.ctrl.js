59c0661ea848df4b46b5b41655b5ae6fcd92c181
kimjihong9 <wlghddl9@gmail.com>
2023-03-26 23:30:56 +0900

    feat. login api 구현

--- backend/controllers/user.ctrl.js
+++ backend/controllers/user.ctrl.js
@@ -1,5 +1,35 @@
+const bcrypt = require('bcrypt');
+const db = require('../config/db');
+
 const login = (req, res) => {
-  res.send('login');
+  const { email, password } = req.body;
+
+  // mysql에서 사용자 정보 조회
+  db.query('SELECT * FROM member WHERE email = ?', [email], (error, result) => {
+    if (error) throw error;
+
+    // 조회된 사용자 정보 없을 때
+    if (result.length === 0) {
+      res.status(401).json({ message: '등록되지 않은 사용자입니다.' });
+      return;
+    }
+
+    // 조회된 사용자 정보와 입력한 비밀번호를 비교
+    const user = result[0];
+    bcrypt.compare(password, user.passwd, (error, isMatch) => {
+      if (error) throw error;
+
+      isMatch = password === user.passwd ? true : false;
+
+      if (isMatch) {
+        res.status(200).json({ message: '로그인 성공' });
+      } else {
+        res
+          .status(401)
+          .json({ message: '사용자 이름 또는 패스워드가 올바르지 않습니다.' });
+      }
+    });
+  });
 };
 
 module.exports = { login };
