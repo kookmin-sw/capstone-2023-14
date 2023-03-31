ab2ad7d01ee4fb2f2a57666b5a21c09ed30491fe
seo5220 <seo5220@naver.com>
2023-03-31 15:57:47 +0900

    pandas csv read 오류 수정

--- DM/csv_to_db.py
+++ DM/csv_to_db.py
@@ -64,7 +64,8 @@ if __name__ == "__main__":
         country_id = res[0][0]
 
         # pandas csv 파일 읽기
-        data = pd.read_csv(crawl_path + file)
+        # Buffer overflow 관련 오류로 lineterminator 파라미터 추가
+        data = pd.read_csv(crawl_path + file, lineterminator='\n'))
         word_set = []
 
         for idx, content in enumerate(data['contents']):
