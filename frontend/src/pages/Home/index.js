3471f48384ae8a4ee7cc558e35329ba54ed06bd7
kimjihong9 <wlghddl9@gmail.com>
2023-05-01 05:33:49 +0900

    fix. 여행지 추천 페이지 데이터 수정

--- frontend/src/pages/Home/index.js
+++ frontend/src/pages/Home/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import Footer from '../../components/Footer/footer';
 import Destination from '../../components/Destination';
 import { Wrap } from './styles';
@@ -9,38 +9,78 @@ import axios from 'axios';
 
 function Home() {
   const navigator = useNavigate();
+  const [recommendList, setRecommendList] = useState([]);
+
+  useEffect(() => {
+    const fetchData = () => {
+      // 추천 받은 여행지. JSON 형태
+      const response = {
+        city1: '보라카이',
+        city2: '도쿄',
+        city3: '방콕',
+      };
+      const cityList = Object.keys(response);
+      cityList.forEach((city) => {
+        addRecommendList(response[city]);
+      });
+    };
+
+    fetchData();
+  }, []);
+
+  useEffect(() => {
+    const getImage = () => {
+      recommendList.forEach(async (destination) => {
+        const response = await axios.post(
+          'http://localhost:5001/api/recommend',
+          {
+            city: destination.title,
+          },
+        );
+        updateRecommendList(destination.title, response.data);
+      });
+    };
+    getImage();
+  }, [recommendList]);
+
+  const addRecommendList = (destination) => {
+    setRecommendList((prevList) => [
+      ...prevList,
+      {
+        title: destination,
+        imgUrl: '',
+        companion: '',
+      },
+    ]);
+  };
+
+  const updateRecommendList = (destination, url) => {
+    setRecommendList(
+      recommendList.map((item) =>
+        item.title === destination ? { ...item, imgUrl: url } : item,
+      ),
+    );
+  };
+
   const handleClickDestination = (event) => {
     event.preventDefault();
     const id = event.currentTarget.querySelector('span').innerText; // 나라명
     navigator(`/detail/${id}`);
   };
 
-  const [destination, setDestination] = useState({
-    title: '방콕',
-    imgUrl: '',
-    companion: '김지홍',
-  });
-
-  axios
-    .post('http://localhost:5001/api/recommend', { city: destination.title })
-    .then((res) => {
-      setDestination({
-        ...destination,
-        imgUrl: res.data,
-      });
-    });
-
   return (
     <div>
       <Wrap>
         <Title margin={'20px 0'}>추천하는 여행지 & 비슷한 사용자</Title>
-        <Destination
-          onClick={handleClickDestination}
-          key={destination.title}
-          title={destination.title}
-          imgUrl={`data:image/jpeg;base64,${destination.imgUrl}`}
-          companion={destination.companion}
-        />
+        {recommendList.map((destination) => (
+          <Destination
+            onClick={handleClickDestination}
+            key={destination.title}
+            title={destination.title}
+            imgUrl={`data:image/jpeg;base64,${destination.imgUrl}`}
+            companion={destination.companion}
+          />
+        ))}
       </Wrap>
       <Footer />
     </div>
