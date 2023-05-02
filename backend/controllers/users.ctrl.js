8c15119fb835559d21a12a91737f8816d50e265f
kimjihong9 <wlghddl9@gmail.com>
2023-05-02 20:28:10 +0900

    fix. 사용자 취향 저장 api 중복 에러 해결

--- backend/controllers/users.ctrl.js
+++ backend/controllers/users.ctrl.js
@@ -2,11 +2,16 @@ import db from '../config/db.js';
 
 const saveTaste = (req, res) => {
   const { email, style, object, preferAge, preferGender } = req.body;
-  console.log(email, style, object, preferAge, preferGender);
+
+  const valueList = [email, style, object, preferAge, preferGender];
 
   db.query(
-    'INSERT INTO member_info (id, style, object, prefer_age, prefer_gender) VALUES (?,?,?,?,?)',
-    [email, style, object, preferAge, preferGender],
+    `INSERT INTO member_info (id, style, object, prefer_age, prefer_gender) VALUES (?,?,?,?,?) ON DUPLICATE KEY UPDATE
+    style = ?,
+    object = ?,
+    prefer_age = ?,
+    prefer_gender = ?`,
+    [...valueList, ...valueList],
     (error, result) => {
       if (error) throw error;
       res.status(201).json({ success: true });
