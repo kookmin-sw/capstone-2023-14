833fee2254d745c473b6ca53f12f92090f6b5447
sanglim <sanglim@kookmin.ac.kr>
2023-05-09 03:49:22 +0900

    ing. 취향정보 저장 기능 구현중 (취향 중복저장 구현필요)

--- frontend/src/components/Taste/taste.js
+++ frontend/src/components/Taste/taste.js
@@ -1,11 +1,11 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 
 import FullButton from '../Buttons/fullButton';
 import StrokeButton from '../Buttons/strokeButton';
 
 import { SmallTitle, Title } from '../Fonts/fonts';
-import { ButtonWrap, Row } from './styles';
+import { ButtonWrap, Options, Row } from './styles';
 import axios from 'axios';
 
 const Taste = (props) => {
@@ -28,6 +28,19 @@ const Taste = (props) => {
     }
   };
 
+  const SaveMyTaste = (e) => {
+    const checked = e.target.checked;
+    const parent = e.target.parentNode;
+    const name = e.target.name;
+    const value = e.target.parentNode.children[1].innerHTML;
+    setUserTaste({ ...userTaste, [name]: value });
+    if (checked) {
+      parent.style.border = '1.5px solid #EF4E3E';
+    } else {
+      parent.style.border = '1.5px solid #7c7c7c';
+    }
+  };
+
   return (
     <div>
       <div>
@@ -38,22 +51,71 @@ const Taste = (props) => {
         <div>
           <SmallTitle margin={'0 0 12px'}>스타일</SmallTitle>
           <Row>
-            <span>계획적</span>
-            <span>즉흥적</span>
-            <span>뚜벅이</span>
-            <span>대중교통</span>
+            <Options>
+              <input
+                type={'checkbox'}
+                name={'style'}
+                onClick={(e) => SaveMyTaste(e)}
+              />
+              <span>계획적</span>
+            </Options>
+            <Options>
+              <input
+                type={'checkbox'}
+                name={'style'}
+                onClick={(e) => SaveMyTaste(e)}
+              />
+              <span>즉흥적</span>
+            </Options>
+            <Options>
+              <input
+                type={'checkbox'}
+                name={'style'}
+                onClick={(e) => SaveMyTaste(e)}
+              />
+              <span>뚜벅이</span>
+            </Options>
+            <Options>
+              <input
+                type={'checkbox'}
+                name={'style'}
+                onClick={(e) => SaveMyTaste(e)}
+              />
+              <span>대중교통</span>
+            </Options>
           </Row>
         </div>
         <div>
           <SmallTitle margin={'0 0 12px'}>목적</SmallTitle>
           <Row>
-            <span>휴양</span>
-            <span>관광</span>
-            <span>쇼핑</span>
-            <span>액티비티</span>
-            <span>음식&카페</span>
-            <span>문화</span>
-            <span>촬영</span>
+            <Options>
+              <input type={'checkbox'} onClick={(e) => SaveMyTaste(e)} />
+              <span>휴양</span>
+            </Options>
+            <Options>
+              <input type={'checkbox'} onClick={(e) => SaveMyTaste(e)} />
+              <span>관광</span>
+            </Options>
+            <Options>
+              <input type={'checkbox'} onClick={(e) => SaveMyTaste(e)} />
+              <span>쇼핑</span>
+            </Options>
+            <Options>
+              <input type={'checkbox'} onClick={(e) => SaveMyTaste(e)} />
+              <span>액티비티</span>
+            </Options>
+            <Options>
+              <input type={'checkbox'} onClick={(e) => SaveMyTaste(e)} />
+              <span>음식&카페</span>
+            </Options>
+            <Options>
+              <input type={'checkbox'} onClick={(e) => SaveMyTaste(e)} />
+              <span>문화</span>
+            </Options>
+            <Options>
+              <input type={'checkbox'} onClick={(e) => SaveMyTaste(e)} />
+              <span>촬영</span>
+            </Options>
           </Row>
         </div>
       </div>
@@ -65,19 +127,43 @@ const Taste = (props) => {
         <div>
           <SmallTitle margin={'0 0 12px'}>연령대</SmallTitle>
           <Row>
-            <span>10대</span>
-            <span>20대</span>
-            <span>30대</span>
-            <span>40대</span>
-            <span>50대</span>
-            <span>상관없음</span>
+            <Options>
+              <input type={'checkbox'} onClick={(e) => SaveMyTaste(e)} />
+              <span>10대</span>
+            </Options>
+            <Options>
+              <input type={'checkbox'} onClick={(e) => SaveMyTaste(e)} />
+              <span>20대</span>
+            </Options>
+            <Options>
+              <input type={'checkbox'} onClick={(e) => SaveMyTaste(e)} />
+              <span>30대</span>
+            </Options>
+            <Options>
+              <input type={'checkbox'} onClick={(e) => SaveMyTaste(e)} />
+              <span>40대</span>
+            </Options>
+            <Options>
+              <input type={'checkbox'} onClick={(e) => SaveMyTaste(e)} />
+              <span>50대</span>
+            </Options>
+            <Options>
+              <input type={'checkbox'} onClick={(e) => SaveMyTaste(e)} />
+              <span>상관없음</span>
+            </Options>
           </Row>
         </div>
         <div>
           <SmallTitle margin={'0 0 12px'}>성별</SmallTitle>
           <Row>
-            <span>동성</span>
-            <span>혼성</span>
+            <Options>
+              <input type={'checkbox'} onClick={(e) => SaveMyTaste(e)} />{' '}
+              <span>동성</span>
+            </Options>
+            <Options>
+              <input type={'checkbox'} onClick={(e) => SaveMyTaste(e)} />{' '}
+              <span>혼성</span>
+            </Options>
           </Row>
         </div>
       </div>
