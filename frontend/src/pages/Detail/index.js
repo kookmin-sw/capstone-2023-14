3c8c5908243dda5d0464265b1d3ac756ac92cfba
kimjihong9 <wlghddl9@gmail.com>
2023-05-10 15:31:58 +0900

    feat. 추천 동행인 api 호출 및 화면 그리기

--- frontend/src/pages/Detail/index.js
+++ frontend/src/pages/Detail/index.js
@@ -9,20 +9,15 @@ import axios from 'axios';
 
 function Detail() {
   const params = useParams();
-  const [isLoading, setIsLoading] = useState(true);
   const destination = params.id;
+  const [userEmail, setUserEmail] = useState('vory');
   const [city, setCity] = useState({
     name: destination,
     info: {},
   });
-  const [companionList, setCompanionList] = useState([
-    { name: '윤서영', mbti: 'ISTP' },
-    { name: '김지홍', mbti: 'ISFJ' },
-    { name: '남상림', mbti: 'ENTJ' },
-    { name: '윤서', mbti: 'IST' },
-    { name: '김지', mbti: 'ISF' },
-    { name: '남상', mbti: 'ENT' },
-  ]);
+  const [cityId, setCityId] = useState(-1);
+  const [companionList, setCompanionList] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
     const fetchData = async () => {
@@ -32,10 +27,20 @@ function Detail() {
         city: destination,
       });
       setCity({ ...city, info: response.data });
+      setCityId(response.data.id);
       setIsLoading(false);
     };
 
+    const fetchCompanion = async () => {
+      setIsLoading(true);
+      const response = await axios.get(
+        `http://3.38.84.113:5000/dm/companion?user_id=${userEmail}&country_id=${cityId}`,
+      );
+      setCompanionList(response.data.result);
+    };
+
     fetchData();
+    fetchCompanion();
   }, []);
 
   if (isLoading) return <div>Loading</div>;
@@ -72,10 +77,18 @@ function Detail() {
           <Title margin={'0 0 20px'}>동행인 추천</Title>
           {companionList.map((companion) => (
             <UserWrap key={companion.name}>
-              <img
-                src="https://mblogthumb-phinf.pstatic.net/MjAxODAzMDNfMjU4/MDAxNTIwMDQxODA4Mjc0.gR3L5xx3IbpACbvRRF9j9xjJmO-EPAY35oF1AdBnDcog.WZyeqFi6cMmH-v-R-ec44Ny6ZgVyAJIYMT78p4Rxbkwg.PNG.osy2201/2_%2850%ED%8D%BC%EC%84%BC%ED%8A%B8_%ED%9A%8C%EC%83%89%29_%ED%9A%8C%EC%83%89_%EB%8B%A8%EC%83%89_%EB%B0%B0%EA%B2%BD%ED%99%94%EB%A9%B4_180303.png?type=w800" // 사용자 프로필 이미지
-                alt="profile"
-              />
+              {companion.profile ? (
+                <img
+                  src={`data:image/jpeg;base64,${companion.profile}`}
+                  alt="profile"
+                />
+              ) : (
+                <img
+                  src="https://mblogthumb-phinf.pstatic.net/MjAxODAzMDNfMjU4/MDAxNTIwMDQxODA4Mjc0.gR3L5xx3IbpACbvRRF9j9xjJmO-EPAY35oF1AdBnDcog.WZyeqFi6cMmH-v-R-ec44Ny6ZgVyAJIYMT78p4Rxbkwg.PNG.osy2201/2_%2850%ED%8D%BC%EC%84%BC%ED%8A%B8_%ED%9A%8C%EC%83%89%29_%ED%9A%8C%EC%83%89_%EB%8B%A8%EC%83%89_%EB%B0%B0%EA%B2%BD%ED%99%94%EB%A9%B4_180303.png?type=w800"
+                  alt="profile"
+                />
+              )}
+
               <Normal>{companion.name}</Normal>
               <Small color={'#D9D9D9'}>{companion.mbti}</Small>
             </UserWrap>
