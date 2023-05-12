f746bd33cbd01bde455a3ec79475ebcdc54a949c
kimjihong9 <wlghddl9@gmail.com>
2023-05-12 22:45:20 +0900

    feat. 게시글 저장 api 호출

--- frontend/src/pages/Board/boardUpload.js
+++ frontend/src/pages/Board/boardUpload.js
@@ -1,15 +1,49 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Header from '../../components/Header/header';
 import { DetailInfo, Textarea, Wrap, WriterInfo } from './styles';
 import { Small, SubTitle } from '../../components/Fonts/fonts';
 import { useNavigate } from 'react-router-dom';
+import axios from 'axios';
 
 function BoardUpload() {
   const navigator = useNavigate();
 
-  const uploadContent = () => {
+  const [saveInfo, setSaveInfo] = useState({
+    email: 'test',
+    content: '',
+    upload_time: new Date(),
+    update_time: new Date(),
+  });
+
+  const uploadContent = async () => {
+    setSaveInfo((prevState) => {
+      return {
+        ...prevState,
+        upload_ime: prevState.upload_time
+          .toISOString()
+          .slice(0, 19)
+          .replace('T', ' '),
+        update_ime: prevState.update_time
+          .toISOString()
+          .slice(0, 19)
+          .replace('T', ' '),
+      };
+    });
+    try {
+      await axios.post('http://localhost:5001/api/board-write', saveInfo);
+    } catch (e) {
+      console.log(e);
+    }
+
     alert('게시글이 작성되었습니다.\n게시판으로 이동합니다.');
     navigator('/board');
+  };
+
+  const handleChangeInput = (event) => {
+    setSaveInfo({
+      ...saveInfo,
+      content: event.target.value,
+    });
   };
 
   return (
@@ -17,7 +51,7 @@ function BoardUpload() {
       <Header title={'board-upload'} onClick={uploadContent} />
       <Wrap>
         <WriterInfo>
-          <img src={''} />
+          <img src={''} alt="" />
           <div>
             <SubTitle margin={'0 0 2px'}>username</SubTitle>
             <DetailInfo>
@@ -35,6 +69,8 @@ function BoardUpload() {
             '광고/홍보 목적의 글 \n' +
             '정치/사회에 대한 의견 및 가치관을 드러내는 글 '
           }
+          onChange={handleChangeInput}
+          value={saveInfo.content}
         />
       </Wrap>
     </div>
