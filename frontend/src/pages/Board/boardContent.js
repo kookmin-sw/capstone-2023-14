81e33cff691ecd07f182d191369d54389458ef30
kimjihong9 <wlghddl9@gmail.com>
2023-05-13 03:12:57 +0900

    feat. 게시글 댓글 전송 후 화면 리렌더링

--- frontend/src/pages/Board/boardContent.js
+++ frontend/src/pages/Board/boardContent.js
@@ -10,10 +10,18 @@ function BoardContent() {
   const [post, setPost] = useState({});
   const [feedComments, setFeedComments] = useState([]);
 
+  const [chat, setChat] = useState({
+    board_id: -1,
+    email: 'user',
+    content: '',
+  });
+
   useEffect(() => {
-    const showPost = () => {
-      setPost({ ...props.state });
-    };
+    setPost({ ...props.state });
+    setChat((chat) => ({ ...chat, board_id: props.state.board_id }));
+  }, [props.state]);
+
+  useEffect(() => {
     const fetchComment = async () => {
       try {
         const response = await axios.post(
@@ -27,48 +35,48 @@ function BoardContent() {
         console.log(e);
       }
     };
-
-    showPost();
     fetchComment();
-  }, []);
+  }, [props.state.board_id]);
 
-  // 전체 채팅을 담는 배열
-  // 유저가 입력한 채팅 내용 저장
-  const [chat, setChat] = useState({
-    userName: 'user',
-    comment: '',
-  });
-  // 메세지 전송
-  const SendChatText = () => {
-    if (chat.comment === '') return;
-    const AllComment = [...feedComments];
-    AllComment.push(chat);
-    setChat({ userName: 'user', comment: '' });
-    setFeedComments(AllComment);
+  const SendChatText = async () => {
+    if (chat.content === '') return;
+
+    const now = new Date().toISOString().slice(0, 19).replace('T', ' ');
+    const chatToBeSent = {
+      ...chat,
+      upload_time: now,
+      update_time: now,
+    };
+
+    try {
+      await axios.post('http://localhost:5001/api/reply-write', chatToBeSent);
+      setChat((chat) => ({ ...chat, content: '' }));
+      // 댓글 보내고 새로 댓글 목록 가져오기
+      const response = await axios.post(
+        'http://localhost:5001/api/get-replyList',
+        {
+          board_id: props.state.board_id,
+        },
+      );
+      setFeedComments(response.data);
+    } catch (e) {
+      console.log(e);
+    }
   };
 
-  // 키보드 키 입력 이벤트
   const HandleKeyDown = (e) => {
-    // isComposing 이 true 이면 글자 조합 중이므로 동작 막기
     if (e.nativeEvent.isComposing) return;
-
-    // shift + enter 개행
     if (e.key === 'Enter' && e.shiftKey) return;
-
-    // enter 입력
     if (e.key === 'Enter' && !e.shiftKey) {
       SendChatText();
       e.preventDefault();
     }
   };
-  // 입력값 변화 시 저장
+
   const handleOnChange = (e) => {
-    const { name, value } = e.target;
-    setChat({
-      ...chat,
-      [name]: value,
-    });
+    setChat((chat) => ({ ...chat, content: e.target.value }));
   };
+
   return (
     <div>
       <Header title={'board-content'} />
@@ -86,14 +94,12 @@ function BoardContent() {
         </WriterInfo>
         <Textarea value={post.content} disabled />
         <div>
-          {feedComments.map((comment) => {
-            return (
-              <div key={comment.reply_id}>
-                <span>{comment.replyer}: </span>
-                <span>{comment.content}</span>
-              </div>
-            );
-          })}
+          {feedComments.map((comment) => (
+            <div key={comment.reply_id}>
+              <span>{comment.replyer}: </span>
+              <span>{comment.content}</span>
+            </div>
+          ))}
         </div>
       </Wrap>
       <Comment>
@@ -104,7 +110,7 @@ function BoardContent() {
             onChange={handleOnChange}
             onKeyDown={HandleKeyDown}
             name={'comment'}
-            value={chat.comment}
+            value={chat.content}
           />
           <button onClick={SendChatText}>전송</button>
         </div>
