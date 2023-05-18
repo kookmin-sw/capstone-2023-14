6317349fa4ed017270d23346dd5ca939542620ab
sanglim <sanglim@kookmin.ac.kr>
2023-05-18 15:32:31 +0900

    style. 미디어쿼리 적용 및 커서포인터 적용

--- frontend/src/pages/Join/join.js
+++ frontend/src/pages/Join/join.js
@@ -116,7 +116,7 @@ function Join() {
       .post('http://localhost:5001/api/signup', signUpData)
       .then((response) => {
         if (response.status === 201) {
-          console.log('회원가입 성공');
+          //console.log('회원가입 성공');
           alert('회원가입이 완료되었습니다. 로그인 페이지로 이동합니다.');
           navigator('/');
         }
--- frontend/src/pages/Login/styles.js
+++ frontend/src/pages/Login/styles.js
@@ -18,11 +18,11 @@ export const Wrap = styled.div`
   > div:first-child {
     width: 100%;
     text-align: center;
-    margin-bottom: 60px;
+    margin-bottom: 12%;
   }
 
   > div > img {
-    width: 200px;
+    width: 50%;
     height: 200px;
   }
 
