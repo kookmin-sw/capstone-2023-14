a2fd79ddf5c292ce747d664a7c5cf4d004a5d6d5
sanglim <sanglim@kookmin.ac.kr>
2023-05-18 21:04:55 +0900

    feat. user login token 을 recoil로 관리하며 localStorage에 저장 기능 구현

--- frontend/src/pages/Login/login.js
+++ frontend/src/pages/Login/login.js
@@ -9,11 +9,12 @@ import { Normal } from '../../components/Fonts/fonts';
 import axios from 'axios';
 
 import { useRecoilState } from 'recoil';
-import { email } from '../../store/userInfo';
+import { email, token } from '../../store/userInfo';
 
 function Login() {
   const navigator = useNavigate();
   const [userEmail, setUserEmail] = useRecoilState(email);
+  const [userToken, setUserToken] = useRecoilState(token);
   const [autoLogin, setAutoLogin] = useState(false);
   const [inputInfo, setInputInfo] = useState({
     email: '',
@@ -35,7 +36,8 @@ function Login() {
         // 로그인 성공했을 때
         if (response.status === 200 && response.data.success) {
           setUserEmail(inputInfo.email);
-          navigator('/home');
+          setUserToken(response.data.token);
+          // navigator('/home');
         }
       })
       .catch((error) => {
